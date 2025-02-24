e023a2acce942602a8690a1f062c149459688ad5
mingdodev <mingdo.dev@gmail.com>
2025-02-25 02:20:14 +0900

    feat: cors 허용 메서드 추가

--- src/main/java/bookhive/bookhiveserver/global/config/WebConfig.java
+++ src/main/java/bookhive/bookhiveserver/global/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("*")
-                .allowedMethods("GET", "POST")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("Authorization", "Content-Type", "Device-Id")
                 .exposedHeaders("Authorization");
     }
