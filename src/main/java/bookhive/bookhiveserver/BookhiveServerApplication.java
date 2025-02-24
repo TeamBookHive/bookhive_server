bd01e4e9c68a287bf1afa04914c3ebbaa0daaa02
mingdodev <mingdo.dev@gmail.com>
2025-02-25 02:25:19 +0900

    feat: 익명 로그인 기능 구현
    - 해당 Device Id에 토큰이 없다면 생성해서 응답
    - 있다면 부여된 토큰 반환

--- src/main/java/bookhive/bookhiveserver/BookhiveServerApplication.java
+++ src/main/java/bookhive/bookhiveserver/BookhiveServerApplication.java
@@ -2,8 +2,10 @@ package bookhive.bookhiveserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class BookhiveServerApplication {
 
     public static void main(String[] args) {
