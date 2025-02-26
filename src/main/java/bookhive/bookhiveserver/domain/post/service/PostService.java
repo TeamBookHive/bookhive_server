0259df5d90e95b8d589b2f7c1bc62e8fe0155bf7
mingdodev <mingdo.dev@gmail.com>
2025-02-26 14:04:35 +0900

    refactor: 예외 처리 코드 변경
    - 중복 코드 개선 필요
    
    #8

--- src/main/java/bookhive/bookhiveserver/domain/post/service/PostService.java
+++ src/main/java/bookhive/bookhiveserver/domain/post/service/PostService.java
@@ -12,6 +12,7 @@ import bookhive.bookhiveserver.domain.user.repository.UserRepository;
 import jakarta.transaction.Transactional;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
@@ -57,21 +58,33 @@ public class PostService {
                 .orElseThrow(() -> new RuntimeException("잘못된 토큰입니다."));
 
         Post currentPost = postRepository.findById(Long.valueOf(postId))
-                .orElseThrow(() -> new RuntimeException("존재하지 않는 게시글입니다."));
+                .orElseThrow(() -> new RuntimeException("존재하지 않는 게시글입니다:" + postId));
+
+        if (!Objects.equals(currentPost.getUser(), user)) {
+            throw new RuntimeException("해당 게시글에 접근할 권한이 없습니다:" + postId);
+        }
 
         Set<Long> currentTagIds = currentPost.getPostTags().stream()
                 .map(postTag -> postTag.getTag().getId())
                 .collect(Collectors.toSet());
 
+        // 새로운 태그들 중 기존에 존재하던 태그 남기기
         List<PostTag> updatedPostTags = currentPost.getPostTags().stream()
                 .filter(postTag -> newTags.stream()
                         .anyMatch(tag -> tag.getId().equals(postTag.getTag().getId())))
                 .collect(Collectors.toList());
 
+        // 새로운 태그들 중 기존에 없던 태그 추가하기
+        // 해당 유저에게 접근 권한이 있는 태그인지 체크하기
         for (Tag tag : newTags) {
-            if (!currentTagIds.contains(tag.getId())) {
-                updatedPostTags.add(new PostTag(currentPost, tag));
+            if (!currentTagIds.contains(tag.getId()))
+                continue;
+
+            if (!Objects.equals(tag.getUser(), user)) {
+                throw new RuntimeException("해당 태그에 접근할 권한이 없습니다:" + tag.getId());
             }
+
+            updatedPostTags.add(new PostTag(currentPost, tag));
         }
 
         currentPost.update(newContent, updatedPostTags);
@@ -85,10 +98,10 @@ public class PostService {
                 .orElseThrow(() -> new RuntimeException("잘못된 토큰입니다."));
 
         Post post = postRepository.findById(Long.valueOf(postId))
-                .orElseThrow(() -> new RuntimeException("존재하지 않는 게시글입니다."));
+                .orElseThrow(() -> new RuntimeException("존재하지 않는 게시글입니다:" + postId));
 
-        if (!post.getUser().equals(user)) {
-            throw new RuntimeException("해당 게시글에 접근할 권한이 없습니다.");
+        if (!Objects.equals(post.getUser(), user)) {
+            throw new RuntimeException("해당 게시글에 접근할 권한이 없습니다:" + postId);
         }
 
         postRepository.deleteById(Long.valueOf(postId));
