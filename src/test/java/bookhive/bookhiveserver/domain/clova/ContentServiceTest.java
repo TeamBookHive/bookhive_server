0de1f9c7a9dfb3cb826966a0288ea2d83321f110
mingdodev <mingdo.dev@gmail.com>
2025-03-04 19:03:35 +0900

    test(Clova): Clova 응답 형식 오류일 때 반환값 테스트
    - 서비스 테스트

---
+++ src/test/java/bookhive/bookhiveserver/domain/clova/ContentServiceTest.java
@@ -0,0 +1,49 @@
+package bookhive.bookhiveserver.domain.clova;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+import bookhive.bookhiveserver.domain.clova.dto.response.RecommendTagResponse;
+import bookhive.bookhiveserver.domain.clova.service.ContentService;
+import bookhive.bookhiveserver.domain.user.entity.User;
+import bookhive.bookhiveserver.domain.user.repository.UserRepository;
+import java.util.List;
+import java.util.Optional;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class ContentServiceTest {
+
+    @Mock
+    private UserRepository userRepository;
+
+    @InjectMocks
+    private ContentService contentService;
+
+    User testUser;
+
+    @BeforeEach
+    void login() {
+        testUser = new User("testDeiceId", "testToken");
+
+        when(userRepository.findByToken("testToken")).thenReturn(Optional.of(testUser));
+    }
+
+    @Test
+    void 잘못된_응답_형식이라면_기본_추천_태그_리스트를_반환한다() {
+        // given
+        String token = testUser.getToken();
+
+        // when
+        List<RecommendTagResponse> result = contentService.createRecommendTagList("잘못된 응답 테스트 입력 값입니다.", token);
+
+        // then
+        assertThat(result).isNotEmpty();
+        assertThat(result).extracting("value").containsExactly("일상", "기타");
+    }
+}
