7a664675dca12963255149f0b45a4df11f104967
mingdodev <mingdo.dev@gmail.com>
2025-03-09 02:43:52 +0900

    feat: AI 검색 기능 구현
    - 총 2회의 API 요청을 보낸다.
    - Json String 형태의 응답을 ObjectMapper로 파싱한다.
    - API 응답에 따라 서버 자체에서 키워드만으로 필터링하거나, AI가 제공해준 관련 키워드 리스트를 통해 아카이브를 필터링한다.
    - 관련 아카이브가 없는 경우는 랜덤으로 3개를 반환한다.
    
    #23

--- src/main/java/bookhive/bookhiveserver/domain/clova/controller/SearchController.java
+++ src/main/java/bookhive/bookhiveserver/domain/clova/controller/SearchController.java
@@ -1,10 +1,12 @@
 package bookhive.bookhiveserver.domain.clova.controller;
 
-import bookhive.bookhiveserver.domain.clova.dto.request.ContentRequest;
+import bookhive.bookhiveserver.domain.clova.dto.request.SearchRequest;
+import bookhive.bookhiveserver.domain.clova.dto.response.SearchTypeResponse;
 import bookhive.bookhiveserver.domain.clova.service.SearchService;
 import bookhive.bookhiveserver.domain.post.dto.PostResponse;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
@@ -21,10 +24,33 @@ public class SearchController {
 
     @PostMapping("/ai-search-posts")
     public ResponseEntity<List<PostResponse>> search(@RequestHeader(value = "Authorization") String token,
-                                                     @RequestBody ContentRequest request) {
+                                           @RequestBody SearchRequest request) {
 
-        List<PostResponse> posts = searchService.getRandomPosts(token);
+        try {
+            log.info( "키워드 검색 시작");
+            SearchTypeResponse searchType = searchService.checkSearchType(request);
+            List<PostResponse> posts;
 
-        return ResponseEntity.ok(posts);
+            System.out.println(searchType.getIsSearch());
+            System.out.println(searchType.getKeyword());
+
+            if (Boolean.parseBoolean(searchType.getIsSearch())) {
+                log.info( "키워드 검색");
+                posts = searchService.searchByKeyword(searchType.getKeyword(), token);
+            } else {
+                log.info( "AI 검색");
+                posts = searchService.searchByAI(request, token);
+            }
+
+            if (posts.isEmpty()) {
+                log.info("맞는 결과 없음");
+                posts = searchService.getRandomPosts(token);
+            }
+
+            return ResponseEntity.ok(posts);
+        } catch (Exception e) {
+            List<PostResponse> randomPosts = searchService.getRandomPosts(token);
+            return ResponseEntity.ok(randomPosts);
+        }
     }
 }
--- src/main/java/bookhive/bookhiveserver/domain/post/repository/PostRepository.java
+++ src/main/java/bookhive/bookhiveserver/domain/post/repository/PostRepository.java
@@ -4,10 +4,14 @@ import bookhive.bookhiveserver.domain.post.entity.Post;
 import bookhive.bookhiveserver.domain.user.entity.User;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
     List<Post> findByUser(User user);
     List<Post> findByUserOrderByCreatedAtDesc(User user);
+
+    @Query("select p from Post p where p.user = :user and p.content like %:keyword%")
+    List<Post> findByUserAndKeyword(User user, String keyword);
 }
