35de15d9bfd6d6d04600e887e43273448901dc76
mingdodev <mingdo.dev@gmail.com>
2025-03-28 03:44:45 +0900

    fix: 프로세스 id 필드가 존재하지 않을 때 대처
    - 아직 클라이언트 배포가 이루어지지 않아 전달되지 않는 필드가 있을 수 있다. 이에 임시로 오류를 방지하기 위한 기본값을 지정한다.

--- src/main/java/bookhive/bookhiveserver/domain/ai/dto/request/ContentRequest.java
+++ src/main/java/bookhive/bookhiveserver/domain/ai/dto/request/ContentRequest.java
@@ -1,7 +1,5 @@
 package bookhive.bookhiveserver.domain.ai.dto.request;
 
-import com.fasterxml.jackson.annotation.JsonSetter;
-import com.fasterxml.jackson.annotation.Nulls;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
@@ -13,7 +11,5 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class ContentRequest {
     private String content;
-
-    @JsonSetter(nulls = Nulls.AS_EMPTY)
-    private String processId;
+    private String processId = "";
 }
--- src/main/java/bookhive/bookhiveserver/domain/post/dto/PostRequest.java
+++ src/main/java/bookhive/bookhiveserver/domain/post/dto/PostRequest.java
@@ -1,8 +1,6 @@
 package bookhive.bookhiveserver.domain.post.dto;
 
 import bookhive.bookhiveserver.domain.tag.dto.request.TagRequest;
-import com.fasterxml.jackson.annotation.JsonSetter;
-import com.fasterxml.jackson.annotation.Nulls;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.Getter;
@@ -11,7 +9,5 @@ import lombok.Getter;
 public class PostRequest {
     private String content;
     private List<TagRequest> tags = new ArrayList<>();
-
-    @JsonSetter(nulls = Nulls.AS_EMPTY)
-    private String processId;
+    private String processId= "";
 }
