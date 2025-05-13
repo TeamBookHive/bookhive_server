c16eecd2639e8afd7b0643d113394cd8ff2ffe06
mingdodev <mingdo.dev@gmail.com>
2025-05-13 13:42:14 +0900

    fix: postgres 예약어 user 회피를 위한 테이블명 변경
    - 테이블명 복수형으로 통일
    
    #45

--- src/main/java/bookhive/bookhiveserver/domain/post/entity/PostTag.java
+++ src/main/java/bookhive/bookhiveserver/domain/post/entity/PostTag.java
@@ -6,6 +6,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.IdClass;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -18,6 +19,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @IdClass(PostTagId.class)
+@Table(name = "post_tags")
 public class PostTag {
 
     @Id
--- src/main/java/bookhive/bookhiveserver/domain/user/entity/User.java
+++ src/main/java/bookhive/bookhiveserver/domain/user/entity/User.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -16,6 +17,7 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
