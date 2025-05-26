66d7e9502249d84d98048decdc47961dd4d0f3ab
mingdodev <mingdo.dev@gmail.com>
2025-05-26 16:34:58 +0900

    fix: 태그 추천 리스트 생성 시 중복 태그 제거
    - Null인 경우 빈 리스트를 반환하도록 예외 처리

--- src/main/java/bookhive/bookhiveserver/domain/ai/service/content/ContentFacade.java
+++ src/main/java/bookhive/bookhiveserver/domain/ai/service/content/ContentFacade.java
@@ -2,8 +2,9 @@ package bookhive.bookhiveserver.domain.ai.service.content;
 
 import bookhive.bookhiveserver.domain.ai.dto.request.RecommendTagsRequest;
 import bookhive.bookhiveserver.domain.ai.dto.response.RecommendTagResponse;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Stream;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
@@ -17,18 +18,19 @@ public class ContentFacade {
     public List<RecommendTagResponse> recommendTagsPersonalized(RecommendTagsRequest request, String token) {
 
         List<String> sortedTags = contentService.sortTagsByContentRelevance(request, token);
-        Mono<List<String>> relevantTagsMono = contentService.recommendRelevantOriginTags(sortedTags, request, token);
-        Mono<List<String>> newTagsMono = contentService.recommendRelevantNewTags(request, token);
+
+        Mono<List<String>> relevantTagsMono = Optional.ofNullable(
+                contentService.recommendRelevantOriginTags(sortedTags, request, token)
+        ).orElse(Mono.just(List.of()));
+
+        Mono<List<String>> newTagsMono = Optional.ofNullable(
+                contentService.recommendRelevantNewTags(request, token)
+        ).orElse(Mono.just(List.of()));
 
         List<String> resultTags = Mono.zip(relevantTagsMono, newTagsMono)
-                .map(tuple -> {
-                    List<String> mergedTags = new ArrayList<>();
-                    List<String> relevantTags = tuple.getT1();
-                    List<String> newTags = tuple.getT2();
-                    mergedTags.addAll(relevantTags);
-                    mergedTags.addAll(newTags);
-                    return mergedTags;
-                })
+                .map(t -> Stream.concat(t.getT1().stream(), t.getT2().stream())
+                        .distinct()
+                        .toList())
                 .block();
 
         return contentService.createRecommendTagList(String.join(", ", resultTags), token);
--- src/main/java/bookhive/bookhiveserver/global/config/WebClientConfig.java
+++ src/main/java/bookhive/bookhiveserver/global/config/WebClientConfig.java
@@ -7,6 +7,8 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Configuration
 public class WebClientConfig {
 
+    // 현재 직접 사용하고 있지 않음, Spring AI를 통해 사용하고 있음
+
     @Bean
     public WebClient webClient(WebClient.Builder builder) {
         return builder.build();
